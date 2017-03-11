e048ff3cdbecf449e879bff60f6ff4d6885ac4c6
appcrashwin7 <xcv678@wp.pl>
2017-03-11 19:52:46 +0100

    add libs

--- Islands/main.cpp
+++ Islands/main.cpp
@@ -1,7 +1,25 @@
-
+#include <SFML/Graphics.hpp>
 
 void main()
 {
+	sf::RenderWindow window(sf::VideoMode(1280, 1024), "Islands", sf::Style::Fullscreen);
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 
 	return;
 }
