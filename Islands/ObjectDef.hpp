966ae9d25a5f4d5c30f8308426c3d191319e49d8
appcrashwin7 <xcv678@wp.pl>
2017-06-29 11:45:55 +0200

    objects has now type add new objects(sapling and chest)

--- Islands/ObjectDef.hpp
+++ Islands/ObjectDef.hpp
@@ -7,8 +7,20 @@
 
 typedef std::pair<std::string, unsigned> Yield;
 
+enum class ObjectType
+{
+	Default,
+	Chest,
+	Tree,
+	Sapling,
+};
+
 class ObjectDef
 {
+protected:
+	ObjectType type;
+
+private:
 	std::string ObjName;
 	sf::Vector2f ObjSize;
 	sf::IntRect ObjTextureCord;
@@ -23,7 +35,8 @@ public:
 		ObjSize(Size),
 		ObjTextureCord(TextureCord),
 		ObjCollisionBox(CollisionBox),
-		ObjYield(yield)
+		ObjYield(yield),
+		type(ObjectType::Default)
 	{}
 	ObjectDef()
 		:ObjDestructible(false),
@@ -31,13 +44,47 @@ public:
 		ObjSize(0,0),
 		ObjTextureCord(0,0,0,0),
 		ObjCollisionBox(0,0,0,0),
-		ObjYield(Yield("",0))
+		ObjYield(Yield("",0)),
+		type(ObjectType::Default)
 	{}
 
+	ObjectType getType() { return type; }
 	std::string getName() { return ObjName; }
 	sf::Vector2f getSize() { return ObjSize; }
 	sf::IntRect getTextureCord() { return ObjTextureCord; }
 	sf::FloatRect getCollisionBox() { return ObjCollisionBox; }
 	bool getDestructible() { return ObjDestructible; }
 	Yield getYield() { return ObjYield; }
+};
+
+class ChestDef : public ObjectDef
+{
+	unsigned ChestCapacity;
+
+public:
+	unsigned getCapacity() { return ChestCapacity; }
+
+	ChestDef(std::string Name, sf::Vector2f Size, sf::IntRect TextureCord, sf::FloatRect CollisionBox,
+		Yield yield, bool Destructible, unsigned Capacity)
+		:ObjectDef(Name, Size, TextureCord, CollisionBox, yield, Destructible)
+	{
+		ChestCapacity = Capacity;
+		type = ObjectType::Chest;
+	}
+};
+
+class Sapling : public ObjectDef
+{
+	float GrowTime;
+
+public:
+	float getGrowTime() { return GrowTime; }
+
+	Sapling(std::string Name, sf::Vector2f Size, sf::IntRect TextureCord, sf::FloatRect CollisionBox,
+		Yield yield, bool Destructible, float TimeForGrow)
+		:ObjectDef(Name, Size, TextureCord, CollisionBox, yield, Destructible)
+	{
+		GrowTime = TimeForGrow;
+		type = ObjectType::Sapling;
+	}
 };
